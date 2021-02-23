fa4abb9c1f6e27444f11fe4004fdf9e8285695c7
Serg <skokorev@gmail.com>
2021-02-23 20:57:22 +0300

    Minor improvements (#7)
    * Added new hook listeners
    
    * Fixed typo
    
    * Added custom field url setting
    
    * Transformed hash to json
    
    * Added custom field update hook listener
    
    * Added https support
    
    Co-authored-by: Сергей Кокорев <kokorev.sergey@alphaopen.com>

--- lib/webhook.rb
+++ lib/webhook.rb
@@ -1,6 +1,7 @@
 require 'webhook/projects_helper_patch'
 require 'webhook/issue_wrapper'
 require 'webhook/webhook_listener'
+require 'webhook/custom_field_wrapper.rb'
 
 module Webhook
 end
---
+++ lib/webhook/custom_field_wrapper.rb
@@ -0,0 +1,15 @@
+module Webhook
+	class Webhook::CustomFieldWrapper
+		def initialize(custom_field)
+			@custom_field = custom_field
+		end
+
+		def to_hash 
+			{
+				id: @custom_field.id,
+				name: @custom_field.name,
+				field_format: @custom_field.field_format
+			}
+		end
+	end
+end
